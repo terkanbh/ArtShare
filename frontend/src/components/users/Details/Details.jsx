5ac049d4420ec57de836992dae070542fdda5921
terkanbh <terkanbh@gmail.com>
2025-04-10 23:15:28 +0300

    frontend: fix user details not showing artworks properly

--- frontend/src/components/users/Details/Details.jsx
+++ frontend/src/components/users/Details/Details.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import { getUserWithArtworks } from '../../../services/usersService.js';
 import ProfileHeader from './ProfileHeader.jsx';
 import ProfileArtworks from './ProfileArtworks.jsx';
-import Container from '../../shared/Container.jsx';
 
 export default function Details() {
   const { id } = useParams();
@@ -20,12 +22,16 @@ export default function Details() {
     userWithArtworks &&
     (
       <Container md={10}>
-        <ProfileHeader user={userWithArtworks.user} /> <br /> <hr />
-        {
-          userWithArtworks.artworks.length !== 0
-            ? <ProfileArtworks artworks={userWithArtworks.artworks} />
-            : <p>{userWithArtworks.user.firstName} has not added any artworks yet!</p>
-        }
+        <Row className="justify-content-center">
+          <Col>
+            <ProfileHeader user={userWithArtworks.user} /> <br /> <hr />
+            {
+              userWithArtworks.artworks.length !== 0
+                ? <ProfileArtworks artworks={userWithArtworks.artworks} />
+                : <p>{userWithArtworks.user.firstName} has not added any artworks yet!</p>
+            }
+          </Col>
+        </Row>
       </Container>
     )
   ));
